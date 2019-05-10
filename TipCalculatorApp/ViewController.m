92b4bec0250ffa1f82cbb7b86dcafe27c1a6400a
LuizRamos <luizramospe@hotmail.com>
2019-05-10 11:23:30 -0400

    First Commmit

---
+++ TipCalculatorApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TipCalculatorApp
+//
+//  Created by Luiz on 5/10/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ TipCalculatorApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculatorApp
+//
+//  Created by Luiz on 5/10/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
